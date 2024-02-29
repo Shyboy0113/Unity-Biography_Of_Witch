b0ce5a88e0717d3a5e1710e58e969bf489235fac
Blang <shyboy0113@gmail.com>
2024-02-29 12:48:24 +0900

    UPR/TMP pro 부가기능 추가 및 UI창 구현

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject[] panels;
+
+
+    public void SetVisible(int idx)
+    {
+        panels[idx].SetActive(true);
+    }
+
+    public void SetInvisible(int idx)
+    {
+        panels[idx].SetActive(false);
+    }
+    
+}
