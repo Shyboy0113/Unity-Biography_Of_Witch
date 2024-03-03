53f976f77e4a7738cab5b9e3043653c9665241f2
Blang <shyboy0113@gmail.com>
2024-03-03 22:53:07 +0900

    눌린 버튼의 알파(투명도)값 조절하는 기능 구현 완료

--- Assets/Scenes/UIManager/Book_Panel.cs
+++ Assets/Scenes/UIManager/Book_Panel.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Book_Panel : MonoBehaviour
 {
 
     public GameObject[] panels;
+
+    public Image[] buttonColor;
 
     public void SetPanelsVisibility(int idx)
     {
@@ -14,8 +17,19 @@ public class Book_Panel : MonoBehaviour
             if (i == idx)
             {
                 panels[i].SetActive(true);
+
+                //눌린 버튼의 투명도 낮춤
+                Color pressedColor = buttonColor[i].color;
+                pressedColor.a = 10f / 255f;
+                buttonColor[i].color = pressedColor;
+
                 continue;
             }
+
+            //안 눌린 버튼의 투명도 원래대로
+            Color relasedColor = buttonColor[i].color;
+            relasedColor.a = 50f / 255f;
+            buttonColor[i].color = relasedColor;
 
             panels[i].SetActive(false);
         }
