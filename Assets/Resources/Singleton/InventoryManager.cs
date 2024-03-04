7954802dd377d090efc46469eb43c592e0462a99
Blang <shyboy0113@gmail.com>
2024-03-05 00:50:53 +0900

    특정 Layer에 있는 오브젝트에 외각선을 추가해주는 기능 추가

---
+++ Assets/Resources/Singleton/InventoryManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : Singleton<InventoryManager>
+{
+
+    public GameObject head;
+    public GameObject top;
+    public GameObject bottom;
+    public GameObject glove;
+    public GameObject shoes;
+
+    public GameObject weapon;
+
+    public GameObject[] accessories;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scenes/CharacterMove.cs
+++ Assets/Scenes/CharacterMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5.0f; // 캐릭터의 이동 속도
+    public float rotationSpeed = 100.0f; // 캐릭터의 회전 속도
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 앞뒤로 이동
+        float translation = Input.GetAxis("Vertical") * speed;
+        // 좌우로 회전
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // 캐릭터를 이동시킨다
+        transform.Translate(0, 0, translation);
+
+        // 캐릭터를 회전시킨다
+        transform.Rotate(0, rotation, 0);
     }
+
+
 }
